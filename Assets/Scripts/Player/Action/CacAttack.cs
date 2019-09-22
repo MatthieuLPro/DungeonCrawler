e9c4ba23fee395b919f13f02a34bb93dae865327
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-09-22 17:18:26 +0200

    add knockback enemy for sword attack player

---
+++ Assets/Scripts/Player/Action/CacAttack.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CacAttack : MonoBehaviour
+{
+    // _KnockTime has to be < MainAttack Time
+    [SerializeField]
+    private float _knockTime    = 0.25f,
+                  _thrust       = 1f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {   
+        if(other.CompareTag("ItemDestructible"))
+            other.GetComponent<DestructibleItem>().Smash();
+        else if (other.CompareTag("Enemy"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+
+            if (enemy == null)
+                return;
+
+            Vector2 difference = enemy.transform.position - transform.position;
+            difference = difference.normalized * _thrust;
+            ChangeEnemyState(enemy);
+            //ChangeEnemyLifeOrMana(enemy);
+            enemy.AddForce(difference, ForceMode2D.Impulse);
+            StartCoroutine((KnockCo(enemy)));
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        yield return new WaitForSeconds(_knockTime);
+
+        enemy.velocity = Vector2.zero;
+        ChangeEnemyState(enemy);
+    }
+
+    private void ChangeEnemyState(Rigidbody2D enemy)
+    {
+        if (enemy.GetComponent<EnemyController>().enabled == true)
+        {
+            enemy.GetComponent<MovingObject>().currentState = ObjectState.knockBack;
+
+            enemy.GetComponent<EnemyController>().enabled = false;
+            enemy.GetComponent<EnemyInteraction>().enabled = false;
+            enemy.GetComponent<MovingObject>().enabled = false;
+
+            enemy.GetComponent<Animator>().SetBool("Moving", false);
+            //enemy.GetComponent<Animator>().SetBool("KnockBacking", true);
+        }
+        else
+        {
+            enemy.GetComponent<EnemyController>().enabled = true;
+            enemy.GetComponent<EnemyInteraction>().enabled = true;
+            enemy.GetComponent<MovingObject>().enabled = true;
+
+            //enemy.GetComponent<Animator>().SetBool("KnockBacking", false);
+            enemy.GetComponent<MovingObject>().currentState = ObjectState.idle;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Action/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MovingObject
+{
+    void FixedUpdate()
+    {
+        PlayerDirection();
+        MainController();
+    }
+
+    private void PlayerDirection()
+    {
+        changePos = Vector3.zero;
+        changePos.x = Input.GetAxisRaw("Horizontal");
+        changePos.y = Input.GetAxisRaw("Vertical");
+    }
+
+    public override void MainController()
+    {
+        if (Input.GetButtonDown("Attack") && currentState != ObjectState.attack)
+            StartCoroutine(MainAttack());
+        else if (changePos != Vector3.zero && currentState != ObjectState.attack)
+            MoveObject();
+        else
+            AnimationIdle();
+    }
+
+    private IEnumerator MainAttack()
+    {
+        DiagonalAttack();
+        anime.SetBool("Attacking", true);
+        currentState = ObjectState.attack;
+        yield return null;
+
+        anime.SetBool("Attacking", false);
+        yield return new WaitForSeconds(.44f);
+
+        currentState = ObjectState.idle;
+    }
+
+    private void DiagonalAttack()
+    {
+        if (changePos.x == 0 || changePos.y == 0)
+            return;
+
+        float animDirectionX = 0f;
+        float animDirectionY = (changePos.y > 0) ? 0f : -1f;
+
+        if (changePos.y > 0)
+            animDirectionX = (changePos.x < 0) ? -1f : 1f;
+
+        anime.SetFloat("DirectionX", animDirectionX);
+        anime.SetFloat("DirectionY", animDirectionY);
+    }
+}
