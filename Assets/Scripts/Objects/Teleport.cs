753a71fd68ab011efbbc68cbd202e7f6cb04c44b
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-10-01 20:50:48 +0200

    add teleporteur features

---
+++ Assets/Scripts/Objects/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [Header("Set Teleport Arrival")]
+    [SerializeField]
+    private GameObject _teleportArrival = null;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+
+        other.GetComponent<Transform>().position = _teleportArrival.GetComponent<Transform>().position;
+    }
+}
