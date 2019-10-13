69780b5cbc7f50f3d376f72ae8324c5cd2cb3a88
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-10-13 17:10:54 +0200

    add stairs and doors first version

---
+++ Assets/Scripts/MapElements/Doors/TeleportDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportDoor : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+
+        other.GetComponent<Transform>().position = other.GetComponent<Transform>().position + FindTeleportPlace();
+    }
+
+    private Vector3 FindTeleportPlace()
+    {
+        Vector3 newPosition = new Vector3(0, 0, 0);
+
+        switch(transform.parent.GetComponent<InteractionDoor>().teleportDirection)
+        {
+            case 0:
+                newPosition += new Vector3(0, 6.2f, 0);
+                break;
+            case 1:
+                newPosition += new Vector3(6.2f, 0, 0);
+                break;
+            case 2:
+                newPosition += new Vector3(0, -6.2f, 0);
+                break;
+            default:
+                newPosition += new Vector3(-6.2f, 0, 0);
+                break;
+        }
+
+        return (newPosition);
+    }
+}
