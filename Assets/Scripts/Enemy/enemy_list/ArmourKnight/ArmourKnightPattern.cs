800a28558dfe5fde26c801c4a24ffee73693c9c8
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-10-27 21:12:32 +0100

    update armour knights need to fix circle pattern

--- Assets/Scripts/Enemy/enemy_list/ArmourKnight/ArmourKnightPattern.cs
+++ Assets/Scripts/Enemy/enemy_list/ArmourKnight/ArmourKnightPattern.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 public class ArmourKnightPattern : MonoBehaviour
 {
     [Header("Speed Settings")]
-    public float speed = .0f;
+    public float speed = 2f;
 
     [Header("Phase Frequence Settings")]
     [SerializeField]
-    private float _wakingTime       = .0f;
-    [SerializeField]
-    private float _phaseTime        = .0f;
-    [SerializeField]
-    private float _transitionTime   = .0f;
+    private float _wakingTime       = 1.5f;
 
     [Header("Wake up Phase Settings")]
     [SerializeField]
@@ -21,27 +17,30 @@ public class ArmourKnightPattern : MonoBehaviour
 
     [Header("Circle Phase Settings")]
     [SerializeField]
-    private float _rotateSpeed = 5f;
+    private float _rotateSpeed = 2f;
     [SerializeField]
     private float _radius = 0.1f;
     [SerializeField]
-    private float _angle = .0f;
+    private Vector3 _center;
 
     [Header("Transition Circle Phase Settings")]
     [SerializeField]
-    private Vector3 _initCirclePos;
+    private Vector3 _initCirclePos = Vector3.zero;
 
     [Header("Transition Line Phase Settings")]
     [SerializeField]
-    private Vector3 _initLinePos;
+    private Vector3 _initLinePos = Vector3.zero;
 
+    private GameObject      _system;
     private GameObject      _parent;
     private Animator        _animator;
     private Rigidbody2D     _rb2d;
 
-    private bool    _coWakingUp     = true;
-    private bool    _coIsWorking    = false;
-    private int     _phase          = 1;
+    private float _angle = .0f;
+    private bool _coWakingUp;
+    
+    [HideInInspector]
+    public bool ready;
 
     // Phase 1 => Circle
     // Phase 2 => Line
@@ -51,9 +50,20 @@ public class ArmourKnightPattern : MonoBehaviour
     /* ************************************************ */
     protected virtual void Start()
     {
+        _system         = transform.parent.transform.parent.gameObject;
         _parent         = transform.parent.gameObject;
         _animator       = _parent.GetComponent<Animator>();
         _rb2d           = _parent.GetComponent<Rigidbody2D>();
+        ready           = false;
+        _coWakingUp     = true;
+
+        _angle = Mathf.Acos((_initCirclePos.x - _center.x) / _radius);
+
+        Debug.Log("(DEBUT) Angle of : " + _parent.gameObject.name + "\n" +
+                  "_initCirclePos.x - _center.x = " + (_initCirclePos.x - _center.x) + "\n" + 
+                  "(_initCirclePos.x - _center.x) / _radius = " + ((_initCirclePos.x - _center.x) / _radius) + "\n" +
+                  "_angle(radian) = " + _angle + "\n" +
+                  "_angle(degree) = " + (_angle * 180 / Mathf.PI));
 
         StartCoroutine(WakingUpCo());
     }
@@ -82,47 +92,24 @@ public class ArmourKnightPattern : MonoBehaviour
     /* ************************************************ */
     private void WakeUpPattern()
     {
-        Vector3 shakePosition = new Vector3(_parent.transform.position.y + _shakeValue,
+        Vector3 shakePosition = new Vector3(_parent.transform.position.x + _shakeValue,
                                             _parent.transform.position.y,
                                             _parent.transform.position.z);
 
         _shakeValue *= -1;
         _parent.transform.position = shakePosition;
     }
+
     private void FirstPattern()
     {
-        if (_phase == 1)
-        {
-            // Transition To Phase 1
-            if (!_coIsWorking)
-                StartCoroutine(TransitionAndPhaseCo(2, _transitionTime));
-            else
-                TransitionPhaseCircleMovement();
-        }
-        else if (_phase == 2)
-        {
-            // Phase 1
-            if (!_coIsWorking)
-                StartCoroutine(TransitionAndPhaseCo(3, _phaseTime));
-            else
-                CirclePhaseMovement();
-        }
-        else if (_phase == 3)
-        {
-            // Transition to Phase 2
-            /*if (!_coIsWorking)
-                StartCoroutine(TransitionAndPhaseCo(4, _transitionTime));
-            else*/
-            TransitionPhaseLineMovement(_initLinePos);
-        }
-        else if (_phase == 4)
-        {
-            // Phase 2
-            if (!_coIsWorking)
-                StartCoroutine(TransitionAndPhaseCo(1, _phaseTime));
-            else
-                LinePhaseMovement();
-        }
+        if (_system.GetComponent<ArmourKnightSystem>().phase == 1)
+            TransitionPhaseMovement(_initCirclePos);
+        else if (_system.GetComponent<ArmourKnightSystem>().phase == 2)
+            CirclePhaseMovement();
+        else if (_system.GetComponent<ArmourKnightSystem>().phase == 3)
+            TransitionPhaseMovement(_initLinePos);
+        else if (_system.GetComponent<ArmourKnightSystem>().phase == 4)
+            LinePhaseMovement();
     }
 
     private void SecondPattern()
@@ -133,23 +120,28 @@ public class ArmourKnightPattern : MonoBehaviour
     /* ************************************************ */
     /* Phase functions */
     /* ************************************************ */
-    // Transition to Circle phase
-    private void TransitionPhaseCircleMovement()
-    {
-        Debug.Log("Transition to Circle Line");
-    }
-
     // Circle movement
     private void CirclePhaseMovement()
     {
         _angle += _rotateSpeed * Time.deltaTime;
 
+        Debug.Log("(AFTER) Angle of : " + _parent.gameObject.name + "\n" +
+                  "_angle = " + _angle + "\n" +
+                  "_angle(degree) = " + (_angle * 180 / Mathf.PI));
+
+
         var offset = new Vector3(Mathf.Sin(_angle) * _radius, Mathf.Cos(_angle) * _radius, _parent.transform.position.z);
-        _parent.transform.position = _parent.transform.position + offset;
+
+        _parent.transform.position = _center + offset;
     }
 
-    // Transition to Line phase
-    private void TransitionPhaseLineMovement(Vector3 targetPos)
+    // Line movement
+    private void LinePhaseMovement(){
+        _rb2d.MovePosition(_parent.transform.position + new Vector3(0, -1.0f, 0) * speed * Time.deltaTime);
+    }
+
+    // Transition phase
+    private void TransitionPhaseMovement(Vector3 targetPos)
     {
         Vector3 newPosition = new Vector3(0, 0, 0);
         if (_parent.transform.position.x > targetPos.x)
@@ -169,19 +161,12 @@ public class ArmourKnightPattern : MonoBehaviour
         _rb2d.MovePosition(_parent.transform.position + newPosition * speed * Time.deltaTime);
 
         if ((_parent.transform.position.x < targetPos.x + 0.1f) && (_parent.transform.position.x > targetPos.x - 0.1f) &&
-            (_parent.transform.position.y < targetPos.y + 0.1f) && (_parent.transform.position.y > targetPos.y - 0.1f))
+            (_parent.transform.position.y < targetPos.y + 0.1f) && (_parent.transform.position.y > targetPos.y - 0.1f) &&
+             !ready)
         {
-            _phase = 4;
+            ready = true;
         }
-
     }
-
-    // Line movement
-    private void LinePhaseMovement()
-    {
-        _rb2d.MovePosition(_parent.transform.position + new Vector3(0, -1.0f, 0) * speed * Time.deltaTime);
-    }
-
 
     /* ************************************************ */
     /* Coroutines */
@@ -194,16 +179,6 @@ public class ArmourKnightPattern : MonoBehaviour
         _animator.SetInteger("Pattern", 1);
         _animator.SetBool("Moving", true);
         _coWakingUp = false;
-    }
-
-
-    // Transition to Phase and phase wait CoRoutine
-    private IEnumerator TransitionAndPhaseCo(int nextPhase, float waitTime)
-    {
-        _coIsWorking = true;
-        yield return new WaitForSeconds(waitTime);
-        
-        _phase          = nextPhase;
-        _coIsWorking    = false;
+        _system.GetComponent<ArmourKnightSystem>().phase = 1;
     }
 }
---
+++ Assets/Scripts/Enemy/enemy_list/ArmourKnight/ArmourKnightSystem.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmourKnightSystem : MonoBehaviour
+{
+
+    [Header("Armour knights List")]
+    [SerializeField]
+    private GameObject[] _armourKnights;
+
+    [Header("Phase Frequence Settings")]
+    [SerializeField]
+    private float _phaseTime        = .0f;
+    [SerializeField]
+    private float _transitionTime   = .0f;
+
+    private bool _coIsWorking;
+
+    public int phase          = 0;
+
+    void Start(){
+        _coIsWorking    = false;
+    }    
+
+    void Update()
+    {
+        if (phase == 1)
+        {
+            if(KnightsTransitionReady() && !_coIsWorking)
+                StartCoroutine(TransitionAndPhaseCo(phase + 1, _phaseTime));
+        }
+        else if (phase == 2)
+        {
+            if (!_coIsWorking)
+                StartCoroutine(TransitionAndPhaseCo(phase + 1, _phaseTime));
+        }
+        else if (phase == 3)
+        {
+            if(KnightsTransitionReady() && !_coIsWorking)
+                StartCoroutine(TransitionAndPhaseCo(phase + 1, _phaseTime));
+        }
+        else if (phase == 4)
+        {
+            if (!_coIsWorking)
+                StartCoroutine(TransitionAndPhaseCo(1, _phaseTime));
+        }
+    }
+
+    private bool KnightsTransitionReady()
+    {
+        for(var i = 0; i < _armourKnights.Length; i++)
+        {
+            if (!_armourKnights[i].transform.GetChild(0).GetComponent<ArmourKnightPattern>().ready)
+                return false;
+        }
+
+        return true;
+    }
+
+    /* ************************************************ */
+    /* Coroutines */
+    /* ************************************************ */
+    // Transition and Phase wait CoRoutine
+    private IEnumerator TransitionAndPhaseCo(int nextPhase, float waitTime)
+    {
+        _coIsWorking = true;
+        yield return new WaitForSeconds(waitTime);
+        
+        for(var i = 0; i < _armourKnights.Length; i++)
+            _armourKnights[i].transform.GetChild(0).GetComponent<ArmourKnightPattern>().ready = false;
+
+        phase          = nextPhase;
+        _coIsWorking    = false;
+    }
+}
