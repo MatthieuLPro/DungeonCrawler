d998aebe1a0318ad939f834443f9f20a935ce728
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-11-09 14:20:10 +0100

    update action attack player

--- Assets/Scripts/PlayerTest/TestPlayer.cs
+++ Assets/Scripts/PlayerTest/TestPlayer.cs
@@ -7,10 +7,12 @@ public class TestPlayer : MonoBehaviour
     private PolygonCollider2D[] _collidersAttacks;
     private Animator _anime;
     private int _currentColliderIndex;
+    private bool _isAttacking;
 
     void Start(){
         _currentColliderIndex = 0;
         _anime                = GetComponent<Animator>();
+        _isAttacking          = false;
     }
 
     // Set Collider for attack Animation
@@ -21,19 +23,18 @@ public class TestPlayer : MonoBehaviour
         _collidersAttacks[_currentColliderIndex].enabled    = true;
     }
 
-    public void SetColliderToNull()
-    {
-        for (var i = 0; i < _collidersAttacks.Length; i++)
-            _collidersAttacks[i].enabled    = false;
-    }
-
     private void SetColliderToAnimation()
     {
+        if (_isAttacking)
+            return;
+
         float   x               = _anime.GetFloat("DirectionX");
         float   y               = _anime.GetFloat("DirectionY");
         string  attackDirection = "";
 
-        _currentColliderIndex = 0;
+        SetColliderToNull();
+        _isAttacking = true;
+
         if (x == 0)
             if (y > 0)
                 attackDirection = "AttackTopTest";
@@ -41,15 +42,40 @@ public class TestPlayer : MonoBehaviour
                 attackDirection = "AttackBotTest";
         else if (x > 0)
             if (y < 0)
+            {
+                Debug.Log("Exit 1");
                 attackDirection = "AttackBotTest";
+            }
             else
+            {
+                Debug.Log("Exit 2");
                 attackDirection = "AttackRightTest";
+            }
         else
             if (y < 0)
+            {
+                Debug.Log("Exit 3");
                 attackDirection = "AttackBotTest";
+            }
             else
+            {
+                Debug.Log("Exit 4");
                 attackDirection = "AttackLeftTest";
+            }
 
         _collidersAttacks = gameObject.transform.Find("ActionTest").transform.Find(attackDirection).GetComponents<PolygonCollider2D>();
+    }
+
+    public void SetColliderToNull()
+    {
+        string[] directions = new string[4] {"AttackTopTest", "AttackRightTest", "AttackBotTest", "AttackLeftTest"};
+
+        _currentColliderIndex = 0;
+        for (var i = 0; i < directions.Length; i++)
+        {
+            PolygonCollider2D[] colliders = gameObject.transform.Find("ActionTest").transform.Find(directions[i]).GetComponents<PolygonCollider2D>();
+            for (var j = 0; j < colliders.Length; j++)
+                colliders[j].enabled    = false;
+        }
     }
 }
