2c164401505b71e5a25fee7f2f508e971d0ffb17
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-12-22 12:36:17 +0100

    init class profile

---
+++ Assets/Scripts/Profile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Profile : MonoBehaviour
+{
+    /* Existe 2 types de data:
+        - Data que l'on conserve (sauvegarde dans un fichier)
+            - nickName
+            - statistics (nombre de km parcouru etc ...)
+        - Data que l'on ne conserver pas
+            - personnage joué
+            - skin utilisé
+    */ 
+    public string nickName;
+    public enum Character {
+        character_1,
+        character_2,
+        character_3
+    };
+
+    void Start()
+    {
+        //Load value from a file
+    }
+
+    void createProfile()
+    {
+        //Create a profile and save it in a file
+    }
+
+    void createTempProfile()
+    {
+        //Create a temporary profile with temporary data
+    }
+
+    void updateProfile()
+    {
+        //Update a profile and save it in a file
+    }
+
+    void updateTempProfile()
+    {
+        //Update a temporary profile with temporary data
+    }
+}
