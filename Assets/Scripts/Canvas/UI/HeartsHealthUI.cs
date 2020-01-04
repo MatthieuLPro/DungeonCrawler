06682658ce5427d2be7333c17de25d73d3d8361b
MatthieuLPro <matthieu.lu.pro@gmail.com>
2020-01-04 11:09:04 +0100

    change UI folder into canvas

---
+++ Assets/Scripts/Canvas/UI/HeartsHealthUI.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartsHealthUI : MonoBehaviour
+{
+    public static HeartsHealthSystem heartsHealthSystemStatic;
+
+    [Header("Heart Sprites")]    
+    [SerializeField]
+    private Sprite heartSpriteFull = null;
+    [SerializeField]
+    private Sprite heartSpriteMid = null;
+    [SerializeField]
+    private Sprite heartSpriteEmpty = null;
+
+    [Header("Attached player")]
+    [SerializeField]
+    private GameObject player = null;
+
+    private List<HeartImage> heartImageList;
+    public HeartsHealthSystem heartsHealthSystem;
+
+    private void Awake(){
+        heartImageList = new List<HeartImage>();
+    }
+
+    private void Start()
+    {
+        HeartsHealthSystem heartsHealthSystem = new HeartsHealthSystem(player.GetComponent<Player>().healthInit);
+        SetHeartsHealthSystem(heartsHealthSystem);
+    }
+
+    public void SetHeartsHealthSystem(HeartsHealthSystem heartsHealthSystem)
+    {
+        List<HeartsHealthSystem.Heart> heartList = heartsHealthSystem.GetHeartList();
+        int heartOffset = 11;
+ 
+        this.heartsHealthSystem = heartsHealthSystem;
+        heartsHealthSystemStatic = heartsHealthSystem;
+        for(int i = 0; i < heartList.Count; i++)
+        {
+            HeartImage newHeart = CreateHeart(new Vector2(i * heartOffset, 0));
+            newHeart.SetValue(heartList[i].GetValue());
+        }
+
+        heartsHealthSystem.OnDamaged += RefreshHearts;
+        heartsHealthSystem.OnHealed += RefreshHearts;
+    }
+
+    private HeartImage CreateHeart(Vector2 anchoredPosition)
+    {
+        GameObject heartGameObject = new GameObject("heart", typeof(Image));
+        Image heartImageUI = heartGameObject.GetComponent<Image>();
+        HeartImage heartImage = new HeartImage(this, heartImageUI);
+
+        HeartPositionAndSize(heartGameObject, anchoredPosition);
+        heartImageList.Add(heartImage);
+        return (heartImage);
+    }
+
+    private void HeartPositionAndSize(GameObject heart, Vector2 anchoredPosition)
+    {
+        heart.transform.SetParent(transform);
+        heart.transform.localPosition = Vector3.zero;
+        heart.transform.localScale = new Vector3(1, 1, 1);
+        heart.GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
+        heart.GetComponent<RectTransform>().sizeDelta = new Vector2(10, 10);
+    }
+
+    private void RefreshHearts(object sender, System.EventArgs e)
+    {
+        for(int i = 0; i < heartImageList.Count; i++)
+        {
+            HeartImage heartImage = heartImageList[i];
+            HeartsHealthSystem.Heart heart = heartsHealthSystem.GetHeartList()[i];
+            heartImage.SetValue(heart.GetValue());
+        }
+
+        if (heartsHealthSystem.HeartEmpty() == true)
+            player.GetComponent<Player>().IsDead();
+    }
+
+    public class HeartImage
+    {
+        private int value;
+        private Image heartImage;
+        private HeartsHealthUI heartsHealthUI;
+
+        public HeartImage(HeartsHealthUI heartsHealthUI, Image heartImage)
+        {
+            this.heartsHealthUI = heartsHealthUI;
+            this.heartImage = heartImage;
+        }
+
+        public void SetValue(int value)
+        {
+            this.value = value;
+            switch(value)
+            {
+                case 0:
+                    this.heartImage.sprite = heartsHealthUI.heartSpriteEmpty;
+                    break;
+                case 1:
+                    this.heartImage.sprite = heartsHealthUI.heartSpriteMid;
+                    break;
+                default:
+                    this.heartImage.sprite = heartsHealthUI.heartSpriteFull;
+                    break;
+            }
+        }
+
+        public int GetValue(){
+            return value;
+        }
+    }
+}
---
+++ Assets/Scripts/Canvas/UI/ManaUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaUI : MonoBehaviour
+{
+    public static ManaSystem manaSystemStatic;
+    public ManaSystem manaSystem;
+
+    [SerializeField]
+    private Sprite _manaBarSprite = null;
+
+    private GameObject _manaBar;
+
+    [Header("Attached player")]
+    [SerializeField]
+    private GameObject player = null;
+
+
+    private void Start(){
+        _manaBar    = new GameObject("manaBar", typeof(Image));
+        manaSystem = new ManaSystem(player.GetComponent<Player>().manaInit);
+        ManaDisplay();
+    }
+
+    private void ManaDisplay()
+    {
+        Image manaBarUI = _manaBar.GetComponent<Image>();
+
+        manaBarUI.sprite = _manaBarSprite;
+        InitManaUI();
+    }
+
+    public void InitManaUI()
+    {
+        int manaValue = manaSystem.GetMana();
+        manaSystemStatic = manaSystem;
+
+        _manaBar.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0);
+        SetPositionManaUI(manaValue);
+    
+        manaSystem.OnDecrease += RefreshMana;
+        manaSystem.OnIncrease += RefreshMana;
+    }
+
+    private void RefreshMana(object sender, System.EventArgs e)
+    {
+        int manaValue = manaSystem.GetMana();
+
+        SetPositionManaUI(manaValue);
+    }
+
+    private void SetPositionManaUI(int manaValue)
+    {
+        _manaBar.transform.SetParent(transform);
+        _manaBar.transform.localPosition = Vector3.zero;
+        _manaBar.transform.localScale = new Vector3(1, 1, 1);
+        _manaBar.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -18);
+        _manaBar.GetComponent<RectTransform>().sizeDelta = new Vector2(10, manaValue);
+    }
+}
