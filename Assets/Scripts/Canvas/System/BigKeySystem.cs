60e7602f07594fdfc364d2fe7671da5ca407d5a8
matthieulpro <matthieu.lu.pro@gmail.com>
2020-02-22 13:05:03 +0100

    init UI big key and reorganize UI ruby and small key

---
+++ Assets/Scripts/Canvas/System/BigKeySystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BigKeySystem : MonoBehaviour
+{
+    private bool _bigKey;
+
+    public BigKeySystem(bool bigKeyInit){
+        _bigKey = bigKeyInit;
+    }
+
+    public int GetValue(){
+        return _bigKey;
+    }
+
+    public void AddSmallKey()
+    {
+        _GetBigKey();
+        OnIncrease(this, EventArgs.Empty);
+    }
+
+    public void RemoveSmallKey()
+    {
+        _LooseBigKey();
+        OnDecrease(this, EventArgs.Empty);
+    }
+
+    private void _LooseBigKey(){
+        _bigKey = false;
+    }
+
+    private void _GetBigKey(){
+        _bigKey = true;
+    }
+}
---
+++ Assets/Scripts/Canvas/UI/BigKeyUI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BigKeyUI : MonoBehaviour
+{
+    public static BigKeySystem bigKeySystemStatic;
+    
+    [HideInInspector]
+    public BigKeySystem bigKeySystem;
+
+    private bool _bigKey;
+    private AudioSource _audio = null;
+
+    [Header("Attached player")]
+    [SerializeField]
+    public GameObject _player;
+    
+
+    private void Start()
+    {
+        smallKeySystem = new SmallKeySystem(_player.GetComponent<Player>().HasBigKey());
+        _audio = GetComponent<AudioSource>();
+        InitSmallKeyUI();
+    }
+
+    public void InitSmallKeyUI()
+    {
+        _bigKey = _player.GetComponent<Player>().HasBigKey();
+        char value = 'X';
+
+        if (_bigKey)
+            value = 'O';
+        GetComponent<Text>().text = _smallKey.ToString();
+        smallKeySystemStatic = smallKeySystem;
+
+        smallKeySystem.OnDecrease += RefreshSmallKey;
+        smallKeySystem.OnIncrease += RefreshSmallKey;
+    }
+
+    private char _GetCharValue()
+    {
+        char value = 'X';
+
+        if (_bigKey) value = 'O';
+        return value;
+    }
+
+    private void RefreshSmallKey(object sender, System.EventArgs e){
+        StartCoroutine(SmallKeyCo());
+    }
+
+    private IEnumerator SmallKeyCo()
+    {
+        int systemValue = smallKeySystem.GetValue();
+        if (_smallKey < systemValue)
+        {
+            while(_smallKey < systemValue)
+            {
+                _smallKey++;
+                GetComponent<Text>().text = _smallKey.ToString();
+                yield return new WaitForSeconds(0.07f);
+            }
+        }
+        else
+        {
+            while(_smallKey > systemValue)
+            {
+                _smallKey--;
+                GetComponent<Text>().text = _smallKey.ToString();
+                yield return new WaitForSeconds(0.07f);
+
+            }
+        }
+    }
+}
