d676e0527ca4030b3e04e5cd7d3007eedda135a7
matthieulpro <matthieu.lu.pro@gmail.com>
2020-02-22 08:28:39 +0100

    update objects

---
+++ Assets/Scripts/PNJ/MovementSystem/Detection/WallDetector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetector : MonoBehaviour
+{
+    BoxCollider2D wallDetector; 
+    
+    void Start(){
+        wallDetector = this.GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Wall"))
+            return;
+
+        
+    }
+}
---
+++ Assets/Scripts/PNJ/MovementSystem/GetDirections/DirectionPatterns/HuntDirections/HuntDirectionTypes/HuntDirectionLines.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuntDirectionLines : HuntDirections
+{
+    override protected Vector2 _GetDirection(Vector2 targetPosition, Vector2 playerColliderSize)
+    {
+        float xPosition = 0.0f;
+        float yPosition = 0.0f;
+
+        if(Random.Range(0, 2) == 1)
+            xPosition = _GetDirectionVariation(targetPosition.x, playerColliderSize.y / 2);
+        else
+            yPosition = _GetDirectionVariation(targetPosition.y, playerColliderSize.x / 2);
+
+        return new Vector2(xPosition, yPosition);
+    }
+}
