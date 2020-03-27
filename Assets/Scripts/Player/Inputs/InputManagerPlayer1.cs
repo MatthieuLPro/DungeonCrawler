6cd1fa81e931ca78d168a697a2df8d47de79aa1f
matthieulpro <matthieu.lu.pro@gmail.com>
2020-03-27 21:54:26 +0100

    update key for p1

--- Assets/Scripts/Player/Inputs/InputManagerPlayer1.cs
+++ Assets/Scripts/Player/Inputs/InputManagerPlayer1.cs
@@ -13,16 +13,14 @@ public class InputManagerPlayer1 : MonoBehaviour
     public static float MainHorizontal()
     {
         float r = 0.0f;
-        r += Input.GetAxis("J_MainHorizontal");
-        r += Input.GetAxis("K_MainHorizontal");
+        r += Input.GetAxis("P1_Horizontal");
         return Mathf.Clamp(r, -1.0f, 1.0f);
     }
 
     public static float MainVertical()
     {
         float r = 0.0f;
-        r += Input.GetAxis("J_MainVertical");
-        r += Input.GetAxis("K_MainVertical");
+        r += Input.GetAxis("P1_Vertical");
         return Mathf.Clamp(r, -1.0f, 1.0f);
     }
 
@@ -33,23 +31,23 @@ public class InputManagerPlayer1 : MonoBehaviour
 
     public static bool AButton()
     {
-        return Input.GetButtonDown("A_Button");
+        return Input.GetButtonDown("P1_Action");
     }
 
     // Action button
     public static bool BButton()
     {
-        return Input.GetButtonDown("P1_action");
+        return Input.GetButtonDown("P1_Attack");
     }
 
     public static bool XButton()
     {
-        return Input.GetButtonDown("X_Button");
+        return Input.GetButtonDown("P1_Consommable");
     }
 
     public static bool YButton()
     {
-        return Input.GetButtonDown("Y_Button");
+        return Input.GetButtonDown("P1_Equipement");
     }
 
     public static bool LButton()
